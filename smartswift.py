b08d777a937b22529780d21a41b871546c6a1331
asafzaf <asaf.zafrir@gmail.com>
2023-12-16 17:01:37 +0200

    sqlite

--- smartswift.py
+++ smartswift.py
@@ -4,7 +4,25 @@ import datetime
 import telepot
 from telepot.loop import MessageLoop
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
+import sqlite3
 
+try:
+    sqliteConnection = sqlite3.connect('swifts.db')
+    cursor = sqliteConnection.cursor()
+    print("Database created and Successfully Connected to SQLite")
+    sqlite_select_Query = "select sqlite_version();"
+    cursor.execute(sqlite_select_Query)
+    record = cursor.fetchall()
+    print("SQLite Database Version is: ", record)
+    cursor.close()
+    
+except sqlite3.Error as error:
+    print("Error while connecting to sqlite", error)
+    
+finally:
+    if sqliteConnection:
+        sqliteConnection.close()
+        print("The SQLite connection is closed")
 
 def handle(msg):
     chat_id = msg['chat']['id']
