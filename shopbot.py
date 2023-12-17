97eca584d78f788b465b6ae683a3a46756d964ca
asafzaf <asaf.zafrir@gmail.com>
2023-12-17 18:54:24 +0200

    start of someting new

---
+++ shopbot.py
@@ -0,0 +1,42 @@
+import time
+import os
+import telepot
+from telepot.loop import MessageLoop
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
+#import config
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+    
+    print('Got command: %s' % command)
+    if command == '/start':
+        start_command(chat_id)
+    elif command == '/shoplist':
+        f = open('shoplist.txt', 'r')
+        bot.sendMessage(chat_id, f)
+        f.close()
+    elif command == '/buyall':
+        f = open('shoplist.txt', 'w').close()
+        bot.sendMessage(chat_id, 'עבודה טובה, הרשימה נמחקה...')
+    else:
+        f = open('shoplist.txt', 'w+')
+        open('s')
+        f.write(command)
+        f.close()
+        bot.sendMessage(chat_id, f'נוסף: {command}')
+
+def start_command(chat_id):
+    bot.sendMessage(chat_id, 'שלום! נא לרשום מוצרים לרשימת קניות..')
+
+bot = telepot.Bot('6880455155:AAFjQrDZ3PCwkayPIEVi0kfK050pHOLe59U')
+if __name__ == "__main__":
+    MessageLoop(bot, handle).run_as_thread()
+    print('I am listening ...')
+
+while 1:
+    time.sleep(10)
+
+
+
+
