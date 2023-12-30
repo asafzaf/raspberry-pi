f04ca79332f24b1341ea3715dcb80f15993a583a
asafzaf <asaf.zafrir@gmail.com>
2023-12-30 22:35:37 +0200

    mm

--- shopbot.py
+++ shopbot.py
@@ -26,7 +26,7 @@ def handle(msg):
     if command == '/start':
         start_command(chat_id)
     elif command == '/shoplist':
-        cursor.execute(f"SELECT * FROM cart_items LEFT JOIN items on cart_items.item_id = items.id WHERE bot_id = {bot_id} and is_bought = False")
+        cursor.execute(f"SELECT items.name FROM cart_items LEFT JOIN items on cart_items.item_id = items.id WHERE bot_id = {bot_id} and is_bought = False")
         new_msg = ''
         print(new_msg)
         res = cursor.fetchall()
@@ -35,7 +35,7 @@ def handle(msg):
         print(len(res))
         if (len(res) != 0):
             for line in res:
-                (id, item_id, amount, bot_id, chat_id, time_added, is_bought, time_bought, item_name) = line
+                (item_name) = line
                 new_msg = new_msg.join(f"\n{item_name}")
         else:
             new_msg = new_msg.join("empty list...")
