dc2e98cb03a4283f766941ded802c6f2d661608d
asafzaf <asaf.zafrir@gmail.com>
2024-01-01 23:59:20 +0200

    ..

--- mybot.py
+++ mybot.py
@@ -7,6 +7,20 @@ from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 import configMyBot
 import urllib.request
 
+import sys
+print(sys.path)
+
+import os
+
+with open('/home/asafz/tasks/output_file.txt', 'w') as f:
+    for key, value in os.environ.items():
+        f.write(f'{key}={value}\n')
+
+with open('/home/asafz/tasks/output_file123.txt', 'w') as f:
+    for path in sys.path:
+        f.write(f"{path}\n")
+
+
 
 """
 After **inserting token** in the source code, run it:
