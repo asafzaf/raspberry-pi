fba5f330f1866a4680353db54ff95630d8a69cef
asafzaf <asaf.zafrir@gmail.com>
2024-01-02 00:09:59 +0200

    //

--- testfile.py
+++ testfile.py
@@ -3,10 +3,10 @@ print(sys.path)
 
 import os
 
-with open('/home/asafz/tasks/output_file.txt', 'w') as f:
+with open('/home/asafz/dev/output_file.txt', 'w') as f:
     for key, value in os.environ.items():
         f.write(f'{key}={value}\n')
 
-with open('/home/asafz/tasks/output_file123.txt', 'w') as f:
+with open('/home/asafz/dev/output_file123.txt', 'w') as f:
     for path in sys.path:
         f.write(f"{path}\n")
